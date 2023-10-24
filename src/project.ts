a332b617428ecf9e1ef51741d9147586cf8ad40e
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-10-24 12:33:38 -0700

    Add error handling to project command

--- src/project.ts
+++ src/project.ts
@@ -7,12 +7,20 @@ function execSync(
   command: string,
   options?: ExecSyncOptionsWithStringEncoding,
 ) {
-  return nodeExecSync(command, { encoding: 'utf8', ...options })
+  return nodeExecSync(command, {
+    encoding: 'utf8',
+    stdio: 'inherit',
+    ...options,
+  })
 }
 
 export default function project({ name }: { name: string }) {
   console.log(`Creating project in ${name}...`)
-  execSync(`npm x --yes -- create-expo@latest --yes --template tabs ${name}`)
-  console.log(`Project created in ${name}! 🎉 Run:`)
-  console.log(`cd ${name} && npm start`)
+  try {
+    execSync(`npm x --yes -- create-expo@latest --yes --template tabs ${name}`)
+    console.log(`Project created in ${name}! 🎉 Run:`)
+    console.log(`cd ${name} && npm start`)
+  } catch {
+    console.error('The project could not be created.')
+  }
 }
