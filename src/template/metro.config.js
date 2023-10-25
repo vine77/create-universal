668e2c7ba76e506e95748960c2f2fd8d88b85fb5
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-10-24 20:00:42 -0700

    Add template files

---
+++ src/template/metro.config.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config')
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+})
+
+const { transformer, resolver } = config
+
+// Add support for importing SVG files
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+}
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
+}
+
+module.exports = config
