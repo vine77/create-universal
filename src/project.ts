51cd22ff44439b581b21a3ce053d21b1a3c2109f
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-10-24 20:02:12 -0700

    Remove folders from expo's tabs template

--- src/project.ts
+++ src/project.ts
@@ -2,6 +2,8 @@ import {
   ExecSyncOptionsWithStringEncoding,
   execSync as nodeExecSync,
 } from 'child_process'
+import { mkdirSync, rmSync } from 'fs'
+import { chdir } from 'process'
 
 function execSync(
   command: string,
@@ -16,11 +18,24 @@ function execSync(
 
 export default function project({ name }: { name: string }) {
   console.log(`Creating project in ${name}...`)
+
   try {
     execSync(`npm x --yes -- create-expo@latest --yes --template tabs ${name}`)
-    console.log(`Project created in ${name}! 🎉 Run:`)
-    console.log(`cd ${name} && npm start`)
+    chdir(`./${name}`)
   } catch {
     console.error('The project could not be created.')
+    process.exit(1)
   }
+
+  try {
+    rmSync('./app', { recursive: true, force: true })
+    rmSync('./components', { recursive: true, force: true })
+    rmSync('./constants', { recursive: true, force: true })
+  } catch {
+    console.error('An error occurred while configuring the project.')
+    process.exit(1)
+  }
+
+  console.log(`Project created in ${name}! 🎉 Run:`)
+  console.log(`cd ${name} && npm start`)
 }
