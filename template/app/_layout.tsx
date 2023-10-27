8c41ba1927d7814262a793a097d49e649a49b4a7
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-10-27 16:00:35 -0700

    Update template

--- template/app/_layout.tsx
+++ template/app/_layout.tsx
@@ -1,14 +1,20 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { useFonts } from 'expo-font'
 import { SplashScreen, Stack } from 'expo-router'
 import { useEffect } from 'react'
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router'
+import { config } from '../src/components/gluestack-ui.config'
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+// Catch errors thrown by the Layout component
+export { ErrorBoundary } from 'expo-router'
+
+// export const unstable_settings = {
+//   // Ensure that reloading keeps back button present
+//   initialRouteName: '/',
+// }
+
+// Prevent splash screen from auto-hiding before asset loading is complete
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
@@ -17,24 +23,22 @@ export default function RootLayout() {
     ...FontAwesome.font,
   })
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree
   useEffect(() => {
     if (error) throw error
   }, [error])
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync()
-    }
+    if (loaded) SplashScreen.hideAsync()
   }, [loaded])
 
   if (!loaded) {
     return null
   }
 
-  return <RootLayoutNav />
-}
-
-function RootLayoutNav() {
-  return <Stack screenOptions={{ headerShown: false }} />
+  return (
+    <GluestackUIProvider config={config}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GluestackUIProvider>
+  )
 }
