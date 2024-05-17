e7c68c765d76d9644628494c09d2b1dce31fb211
Nathan Ward <175123+vine77@users.noreply.github.com>
2024-05-17 15:30:56 -0700

    Fix bug installing devDependencies in src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -115,10 +115,7 @@ export default async function app({ name }: { name?: string } = {}) {
 
   // Install dev dependencies
   try {
-    execSync(
-      `npm x -y -- expo install ${devDependencies.join(' ')} -- -D'
-      }`,
-    )
+    execSync(`npm x -y -- expo install ${devDependencies.join(' ')} -- -D`)
   } catch {
     console.error('An error occurred while installing dev dependencies.')
     exit(1)
