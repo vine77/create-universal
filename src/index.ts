2f052e495f186b47ca7764a6a7a4aad08c501621
Nathan Ward <175123+vine77@users.noreply.github.com>
2024-12-31 17:24:19 -0800

    Upgrade commander to v13

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { Command } from '@commander-js/extra-typings'
+import { program } from '@commander-js/extra-typings'
 
 import app from './app.js'
 import { packageDescription, packageVersion } from './helpers.js'
 
 export default async function main() {
-  const program = new Command()
-
   program
     .name('npm create universal')
     .version(packageVersion, '-v, --version')
